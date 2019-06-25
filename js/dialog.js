06bdd255c2a28c5b24139c3506c564bdce4c1546
Картавый Артем <kartaviy_arteom@mail.ru>
2019-06-25 22:04:00 +0300

    проверка в инвенторе ли элемент

--- js/dialog.js
+++ js/dialog.js
@@ -5,10 +5,29 @@
   var dialog = setup.querySelector('.upload');
   var artifactsShop = setup.querySelector('.setup-artifacts-shop');
 
+  var inventory = setup.querySelector('.setup-artifacts');
+  var INVENORY_SIZES = {
+    width: 260,
+    height: 260,
+    cell : {
+      width: 65,
+      height: 65
+    }
+  };
+
+
+
   artifactsShop.addEventListener('mousedown', function (evt) {
     if (evt.target.localName === 'img') {
       var magicItem = evt.target;
       magicItem.style.position = 'absolute';
+      var inventoryCoordinates = {
+        x: inventory.offsetTop,
+        y: inventory.offsetLeft
+      };
+
+
+
 
       var startСoordinates = {
         x: evt.clientX,
@@ -28,19 +47,29 @@
           x: moveEvt.clientX,
           y: moveEvt.clientY
         };
+
         magicItem.style.top = (magicItem.offsetTop - shift.y) + 'px';
         magicItem.style.left = (magicItem.offsetLeft - shift.x) + 'px';
+
+          /*if (moveEvt.clientY > inventory.clientY && moveEvt.clientY < inventory.clientY + INVENORY_SIZES.height) {*/
       };
 
       var onMouseUp = function (upEvt) {
         upEvt.preventDefault();
 
+
         document.removeEventListener('mousemove', onMouseMove);
         document.removeEventListener('mouseup', onMouseUp);
+        if (magicItem.offsetLeft > inventoryCoordinates.x && magicItem.offsetLeft < inventoryCoordinates.x + INVENORY_SIZES.width) {
+          magicItem.style.top = 0 + 'px';
+          console.log(magicItem.style.top);
+          magicItem.style.left = 0 + 'px';
+        };
       };
 
       document.addEventListener('mousemove', onMouseMove);
       document.addEventListener('mouseup', onMouseUp);
+
     }
   });
 
