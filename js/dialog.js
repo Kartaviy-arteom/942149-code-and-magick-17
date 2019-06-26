709e0853e8bc9adb88ab0c5e8746b983e6853980
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2019-06-26 10:57:06 +0300

    перемещение предметов в инвентарь
    возврат элементов при перемещении мимо инвентаря
    выравнивание элементов в инвентаре.

--- js/dialog.js
+++ js/dialog.js
@@ -3,41 +3,107 @@
 (function () {
   var setup = document.querySelector('.setup');
   var dialog = setup.querySelector('.upload');
-  var artifactsShop = setup.querySelector('.setup-artifacts-shop');
 
-  var inventory = setup.querySelector('.setup-artifacts');
-  var INVENORY_SIZES = {
-    width: 260,
-    height: 260,
-    cell : {
-      width: 65,
-      height: 65
-    }
-  };
+  (function () {
+    var artifactsShop = setup.querySelector('.setup-artifacts-shop');
+    var inventory = setup.querySelector('.setup-artifacts');
+    var INVENORY_SIZES = {
+      width: 260,
+      height: 260,
+      cell : {
+        width: 65,
+        height: 65
+      }
+    };
+
+    artifactsShop.addEventListener('mousedown', function (evt) {
+      if (evt.target.localName === 'img') {
+        var magicItem = evt.target;
+        magicItem.style.position = 'absolute';
+        var inventoryCoordinates = {
+          x: inventory.getBoundingClientRect().left,
+          y: inventory.getBoundingClientRect().top
+        };
+
+        var magicItemCoordinates = {
+          x: magicItem.getBoundingClientRect().left,
+          y: magicItem.getBoundingClientRect().top
+        };
+
+        var startСoordinates = {
+          x: evt.clientX,
+          y: evt.clientY
+        };
+
+        var onMouseMove = function (moveEvt) {
+          moveEvt.preventDefault();
 
 
+          var shift = {
+            x: startСoordinates.x - moveEvt.clientX,
+            y: startСoordinates.y - moveEvt.clientY
+          };
 
-  artifactsShop.addEventListener('mousedown', function (evt) {
-    if (evt.target.localName === 'img') {
-      var magicItem = evt.target;
-      magicItem.style.position = 'absolute';
-      var inventoryCoordinates = {
-        x: inventory.offsetTop,
-        y: inventory.offsetLeft
-      };
+          startСoordinates = {
+            x: moveEvt.clientX,
+            y: moveEvt.clientY
+          };
+
+          magicItem.style.top = (magicItem.offsetTop - shift.y) + 'px';
+          magicItem.style.left = (magicItem.offsetLeft - shift.x) + 'px';
+        };
+
+        var onMouseUp = function (upEvt) {
+          upEvt.preventDefault();
 
 
+          document.removeEventListener('mousemove', onMouseMove);
+          document.removeEventListener('mouseup', onMouseUp);
+          // по оси х бросаем мимо инвенторя
+          if (magicItem.getBoundingClientRect().left < inventoryCoordinates.x || magicItem.getBoundingClientRect().left > inventoryCoordinates.x + INVENORY_SIZES.width) {
+            magicItem.style.left = (magicItem.offsetLeft - (magicItem.getBoundingClientRect().left - magicItemCoordinates.x)) + 'px';
+            magicItem.style.top = (magicItem.offsetTop - (magicItem.getBoundingClientRect().top - magicItemCoordinates.y)) + 'px';
+          }
+          // по оси y мимо инвенторя
 
+          if (magicItem.getBoundingClientRect().top < inventoryCoordinates.y || magicItem.getBoundingClientRect().top > inventoryCoordinates.y + INVENORY_SIZES.height) {
+            magicItem.style.left = (magicItem.offsetLeft - (magicItem.getBoundingClientRect().left - magicItemCoordinates.x)) + 'px';
+            magicItem.style.top = (magicItem.offsetTop - (magicItem.getBoundingClientRect().top - magicItemCoordinates.y)) + 'px';
+          }
+
+          else {
+            // выравнивание по x
+            var gapX = magicItem.getBoundingClientRect().left  - inventoryCoordinates.x;
+            var magicItemShiftX = gapX % INVENORY_SIZES.cell.width;
+            magicItem.style.left = (magicItem.offsetLeft - magicItemShiftX) + 'px';
+            // выравнивание по y
+            var gapY = magicItem.getBoundingClientRect().top  - inventoryCoordinates.y;
+            var magicItemShiftY = gapY % INVENORY_SIZES.cell.height;
+            magicItem.style.top = (magicItem.offsetTop - magicItemShiftY) + 'px';
+          }
+        };
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+      }
+    });
+  })();
+
+  // Движение диалогового окна
+
+  (function () {
+    dialog.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
 
       var startСoordinates = {
         x: evt.clientX,
         y: evt.clientY
       };
+      var dragged = false;
 
       var onMouseMove = function (moveEvt) {
         moveEvt.preventDefault();
 
-
+        dragged = true;
         var shift = {
           x: startСoordinates.x - moveEvt.clientX,
           y: startСoordinates.y - moveEvt.clientY
@@ -48,78 +114,28 @@
           y: moveEvt.clientY
         };
 
-        magicItem.style.top = (magicItem.offsetTop - shift.y) + 'px';
-        magicItem.style.left = (magicItem.offsetLeft - shift.x) + 'px';
-
-          /*if (moveEvt.clientY > inventory.clientY && moveEvt.clientY < inventory.clientY + INVENORY_SIZES.height) {*/
+        setup.style.top = (setup.offsetTop - shift.y) + 'px';
+        setup.style.left = (setup.offsetLeft - shift.x) + 'px';
       };
 
       var onMouseUp = function (upEvt) {
         upEvt.preventDefault();
 
-
         document.removeEventListener('mousemove', onMouseMove);
         document.removeEventListener('mouseup', onMouseUp);
-        if (magicItem.offsetLeft > inventoryCoordinates.x && magicItem.offsetLeft < inventoryCoordinates.x + INVENORY_SIZES.width) {
-          magicItem.style.top = 0 + 'px';
-          console.log(magicItem.style.top);
-          magicItem.style.left = 0 + 'px';
-        };
+
+        if (dragged) {
+          var onClickPreventDefault = function (evt) {
+            evt.preventDefault();
+            dialog.removeEventListener('click', onClickPreventDefault);
+          };
+          dialog.addEventListener('click', onClickPreventDefault);
+        }
+
       };
 
       document.addEventListener('mousemove', onMouseMove);
       document.addEventListener('mouseup', onMouseUp);
-
-    }
-  });
-
-  // Движение диалогового окна
-
-  dialog.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-
-    var startСoordinates = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-    var dragged = false;
-
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      dragged = true;
-      var shift = {
-        x: startСoordinates.x - moveEvt.clientX,
-        y: startСoordinates.y - moveEvt.clientY
-      };
-
-      startСoordinates = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      setup.style.top = (setup.offsetTop - shift.y) + 'px';
-      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-
-      if (dragged) {
-        var onClickPreventDefault = function (evt) {
-          evt.preventDefault();
-          dialog.removeEventListener('click', onClickPreventDefault);
-        };
-        dialog.addEventListener('click', onClickPreventDefault);
-      }
-
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
-
+    });
+  })();
 })();
